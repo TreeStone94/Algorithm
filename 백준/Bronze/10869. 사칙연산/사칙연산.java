bd439a55d77a5acc341ccd61c22ef0e03ca6431e
treestone <100207991+TreeStone94@users.noreply.github.com>
2025-02-18 22:51:13 +0900

    [Bronze V] Title: 사칙연산, Time: 176 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main{
+    public static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        int num1,num2;
+        num1 = scan.nextInt();
+        num2 = scan.nextInt();
+        
+        System.out.printf("%d\n", num1+num2);
+        System.out.printf("%d\n", num1-num2);
+        System.out.printf("%d\n", num1*num2);
+        System.out.printf("%d\n", num1/num2);
+        System.out.printf("%d\n", num1%num2);
+    }
+}
