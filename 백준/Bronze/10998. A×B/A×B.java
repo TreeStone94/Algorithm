b1f0cff637efea62a66a3b60cefce6d5796e16ff
treestone <100207991+TreeStone94@users.noreply.github.com>
2025-02-18 22:43:51 +0900

    [Bronze V] Title: A×B, Time: 112 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main{
+    public static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        int num1,num2;
+        num1 = scan.nextInt();
+        num2 = scan.nextInt();
+        
+        System.out.printf("%d\n", num1*num2);
+    }
+}
