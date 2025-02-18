17e839e2d9adefea7c0b75ab55aad4a13d0e9498
treestone <100207991+TreeStone94@users.noreply.github.com>
2025-02-19 00:12:19 +0900

    [Bronze V] Title: ??!, Time: 168 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main{
+    public static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        String str  = scan.next();
+        str += "??!";
+        System.out.println(str);
+
+    }
+}
