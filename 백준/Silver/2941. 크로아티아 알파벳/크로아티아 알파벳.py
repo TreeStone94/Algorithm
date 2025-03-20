33699175f4af48206b815ab8f2ba7e969ce6778b
treestone <100207991+TreeStone94@users.noreply.github.com>
2025-03-20 23:37:55 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -2,11 +2,8 @@ s = {"c=": "č", "c-":"ć", "dz=": "dž", "d-":"đ", "lj": "lj", "nj":"nj" , "s=
 
 printS = input()
 
-cnt = 0
 for x in s:
-    if printS.count(x) > 0:
-        cnt += printS.count(x)
-        printS = printS.replace(x, "//")
+    printS = printS.replace(x, "*")
 
-printS = printS.replace("//", "")
-print(cnt + len(printS))
+
+print(len(printS))
