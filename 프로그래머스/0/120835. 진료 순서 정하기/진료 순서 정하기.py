64abb5a7dad062907507c1ff2fed83e057a4fc54
treestone <100207991+TreeStone94@users.noreply.github.com>
2025-04-29 00:02:43 +0900

    Update 진료 순서 정하기.py

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
@@ -2,8 +2,6 @@ def solution(emergency):
     answer = []
     es = sorted(emergency,reverse=True)
     for e in emergency:
-        for i in range(len(emergency)):
-            if es[i] == e:
-                answer.append(i+1)
+        answer.append(es.index(e)+1)
             
     return answer
