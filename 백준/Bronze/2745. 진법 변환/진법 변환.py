07b7ec84be9e09c1f37b34a9a4a5d40fe94ec179
treestone <100207991+TreeStone94@users.noreply.github.com>
2025-05-02 19:59:56 +0900

    Update 진법 변환.py

--- 백준/Bronze/2745. 진법 변환/진법 변환.py
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -1,19 +1,12 @@
-numbers = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+ap = {str(i): i for i in range(10)}
+ap.update({chr(55 + i): i for i in range(10, 36)})
+
+
 n, b = input().split()
-
 n = n[::-1]
-answer = 0
-index = 0
-for number in n:
-    aa = [int(b)] * index
-    x = 1
-    for a in aa:
-        x *= a
 
-    if numbers.count(number) > 0:
-        answer += (ord(number) - 55) * x
-    else:
-        answer += int(number) * x
-    index += 1
+answer = 0
+for i in range(len(n)):
+    answer += ap[n[i]] * int(b) ** i
 
 print(answer)
