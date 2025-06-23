c813e3804f4671838d395e4a214e0659a268f68a
treestone <100207991+TreeStone94@users.noreply.github.com>
2025-06-23 23:30:05 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 484 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
@@ -3,6 +3,4 @@ n = int(input())
 for _ in range(n):
     srr = list(input().split())
     for s in srr:
-        for i in range(len(s)-1,-1, -1):
-            print(s[i], end="")
-        print(end=" ")
+       print(s[::-1],end=" ")
