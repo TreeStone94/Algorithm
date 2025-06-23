d8ccda06fa6a7143ebd11dc0b3c340a386cabb53
treestone <100207991+TreeStone94@users.noreply.github.com>
2025-06-23 23:06:20 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -3,33 +3,27 @@ n = int(input())
 
 arr = []
 
-def empty(arr):
-    if len(arr) == 0:
-        return True
-    else:
-        return False
-
 for _ in range(n):
-    a = list(sys.stdin.readline().split())
+    a = sys.stdin.readline().split()
 
     cmd = a[0]
     if cmd == 'push':
         arr.append(int(a[1]))
     elif cmd == 'pop':
-        if empty(arr):
+        if not arr:
             print(-1)
         else:
             print(arr.pop())
     elif cmd == 'size':
         print(len(arr))
     elif cmd == 'empty':
-        if empty(arr):
+        if not arr:
             print(1)
         else:
             print(0)
     elif cmd == 'top':
-        if empty(arr):
+        if not arr:
             print(-1)
         else:
-            print(arr[len(arr)-1])
+            print(arr[-1])
 
