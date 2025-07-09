49397c9613cfc3bdc1557979bdaa6af1a921876d
treestone <100207991+TreeStone94@users.noreply.github.com>
2025-07-09 19:27:23 +0900

    [Silver II] Title: 에디터, Time: 360 ms, Memory: 38732 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.py
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -1,51 +1,27 @@
-from sys import stdin
+import sys
 
-# 연결리스트의 노드 클래스
-class ListNode:
-    def __init__(self, val, prev, next):
-        self.val = val
-        self.prev = prev
-        self.next = next
+leftStack = list(sys.stdin.readline().strip())
+rightStack = []
+n = int(sys.stdin.readline().strip())
 
-# 연결리스트의 head
-head = ListNode('dummy', None, None)
-curr = head
+for _ in range(n):
+    cmd = sys.stdin.readline().split()
 
-init = input() # 초기 문자열
-for i in range(len(init)): # 초기 문자열을 연결리스트로 만들어 둠
-    new_node = ListNode(init[i], None, None)
-    curr.next = new_node
-    new_node.prev = curr
-    curr = new_node
 
-# 명령어들 처리
-for _ in range(int(input())):
-    command = stdin.readline().rstrip()
-    if command == 'L':
-        if curr.val != 'dummy':
-            curr = curr.prev
-    elif command == 'D':
-        if curr.next:
-            curr = curr.next
-    elif command == 'B':
-        if curr.val != 'dummy':
-            curr = curr.prev
-            if curr.next.next:
-                curr.next = curr.next.next
-                curr.next.prev = curr
-            else:
-                curr.next = None
-    else:
-        new_node = ListNode(command[-1], None, None)
-        if curr.next:
-            new_node.next = curr.next
-            curr.next.prev = new_node
-        curr.next = new_node
-        new_node.prev = curr
-        curr = new_node
+    if cmd[0] == 'L' and leftStack:
+        s = leftStack.pop()
+        rightStack.append(s)
+    elif cmd[0] == 'D' and rightStack:
+        s = rightStack.pop()
+        leftStack.append(s)
+    elif cmd[0] == 'B' and leftStack:
+        leftStack.pop()
+    elif cmd[0] == 'P':
+        leftStack.append(cmd[1])
 
-# head 다음 노드부터 출력
-print_node = head.next
-while print_node:
-    print(print_node.val, end='')
-    print_node = print_node.next
+    # yxz
+    #
+print("".join(leftStack), end="")
+
+while rightStack:
+    print(rightStack.pop(), end="")
