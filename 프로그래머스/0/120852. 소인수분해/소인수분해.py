80960c52922d8313169344f991f6b8029b2d22be
treestone <100207991+TreeStone94@users.noreply.github.com>
2025-07-19 00:10:01 +0900

    [level 0] Title: 소인수분해, Time: 1373.53 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.py
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.py
@@ -1,18 +1,16 @@
 def solution(n):
     answer = []
     number = 2
-    while n > 1:
+    while n >= number:
         if n % number == 0:
             answer.append(number)
             n = n // number
         else:
             number += 1
-            while True:
-                # 소수인지 판별
-                for i in range(2, int(number ** 0.5) + 1):
-                    if number % i == 0:
-                        number += 1
-                        break
-                else:
-                    break  # 소수라면 while 탈출
+            count = 0
+            for i in range(2, number + 1):
+                if number % i == 0:
+                    count += 1
+            if count > 1:
+                number += 1
     return sorted(list(set(answer)))
