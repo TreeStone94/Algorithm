598c7ee4317d083cf0174c8f404f1e9c73eb1366
treestone <100207991+TreeStone94@users.noreply.github.com>
2025-07-20 01:16:06 +0900

    [Silver III] Title: 소수 구하기, Time: 380 ms, Memory: 40084 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,17 @@
+m, n = map(int,input().split())
+
+prime = [True] * (n+1)
+prime[0] = False
+prime[1] = False
+
+for i in range(2,n+1):
+    if prime[i]:
+        for j in range(i*i, n+1, i):
+            prime[j] = False
+
+
+for i in range(m,n+1):
+    if prime[i]:
+        print(i)
+
+
