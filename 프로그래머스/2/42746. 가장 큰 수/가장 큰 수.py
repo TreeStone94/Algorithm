312ef99723f97044f4c0e6945df1567b937808ad
treestone <100207991+TreeStone94@users.noreply.github.com>
2025-10-25 23:14:41 +0900

    [level 2] Title: 가장 큰 수, Time: 61.45 ms, Memory: 26.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -0,0 +1,10 @@
+def solution(numbers):
+    answer = ''
+    numbers = list(map(str, numbers))
+    numbers = sorted(numbers, key = lambda x: (x*4)[:4], reverse=True)
+    answer = ''.join(numbers)
+    if answer[0] == '0':
+        return '0'
+    else:
+        return answer
+    return answer
