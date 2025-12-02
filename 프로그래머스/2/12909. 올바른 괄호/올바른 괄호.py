bc9f775ec713ac88124ea26af9ee3ae26aa5ac56
treestone <100207991+TreeStone94@users.noreply.github.com>
2025-12-02 20:23:39 +0900

    [level 2] Title: 올바른 괄호, Time: 10.23 ms, Memory: 9.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,16 +1,28 @@
 def solution(s):
-    stack = list()
-    for ch in list(s):
-        if ch == '(':
-            stack.append(ch)
-        elif len(stack) == 0:
-            return False
-        else:    
-            left = stack.pop()
-            if ch != ')' and left == '(':
-                return False
+    answer = True
     
-    if len(stack) == 0:
-        return True
-    else:
-        return False
+    """
+        ( 나왔다면 )가 있어야한다.
+        1. stack 을 사용해서 ( 넣기
+        2.1 ) 나오면 pop()
+        2.2 pop()할 stack이 없으면 false
+        3. stack에 빈 배열이 아니면 false
+    """
+    
+    stacks = []
+    for stack in s:
+        if stack == '(':
+            stacks.append(stack)
+        else:
+            if len(stacks) == 0:
+                answer = False
+                break
+            elif stacks.pop() == '(' and stack != ')':
+                answer = False
+                break
+                
+    if len(stacks) != 0:
+        answer = False
+        
+
+    return answer
