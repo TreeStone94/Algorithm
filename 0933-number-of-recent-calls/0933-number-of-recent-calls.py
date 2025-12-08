d77c9cc0e727636dad11a5f73c328acdb8ed5865
treestone <100207991+TreeStone94@users.noreply.github.com>
2025-12-08 22:30:03 +0900

    Time: 2119 ms (12.19%), Space: 23 MB (75.45%) - LeetHub

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+class RecentCounter:
+    """
+    RecentCounter 클래스는 특정 시간 범위 내의 최근 요청 횟수를 계산합니다.
+
+    RecentCounter 클래스를 구현하세요:
+    RecentCounter() 최근 요청 횟수를 0으로 초기화합니다.
+    int ping(int t) 시간 t(밀리초 단위)에 새로운 요청을 추가하고, 최근 3000밀리초(새로운 요청 포함) 동안 발생한 요청 횟수를 반환합니다. 구체적으로, [t -    3000, t] 구간(포함) 내에서 발생한 요청 수를 반환합니다.
+    모든 ping 호출은 이전 호출보다 반드시 더 큰 t 값을 사용합니다.
+    """
+
+    def __init__(self):
+        self.requests = deque()
+
+    def ping(self, t: int) -> int:
+        # 요청수 저장
+        self.requests.append(t)
+        # 카운트 선언
+        count = 0
+
+        # t -3000 보다 첫번째 큐가 작으면 pop
+        if self.requests and self.requests[0] < t -3000:
+            self.requests.popleft()
+
+        # [t- 3000, t] 범위에 있는 카운트 계산
+        for request in self.requests :
+            if t - 3000 <= request <= t:
+                count +=1
+
+        # 카운트 리턴
+        return count
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
