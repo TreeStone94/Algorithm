5520aa01c89307dbaa44b25e93d465c7f056988c
treestone <100207991+TreeStone94@users.noreply.github.com>
2026-01-16 00:15:30 +0900

    [level 2] Title: 타겟 넘버, Time: 392.73 ms, Memory: 9.23 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,11 @@
+def solution(numbers, target):
+    def dfs(index, current_sum):
+        if index == len(numbers):
+            return 1 if current_sum == target else 0
+        count = 0
+        count += dfs(index+1, current_sum + numbers[index])
+        
+        count += dfs(index+1, current_sum - numbers[index])
+        
+        return count
+    return dfs(0,0)
