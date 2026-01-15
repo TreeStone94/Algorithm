9940f7795a3760b9366ad5a747bf55e94ce2e741
treestone <100207991+TreeStone94@users.noreply.github.com>
2026-01-15 22:52:47 +0900

    [level 2] Title: 전화번호 목록, Time: 442.48 ms, Memory: 45.8 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,17 +1,13 @@
-def solution(phone_book):
-    answer = True
-    # 현재 번호가 다른 번호의 앞에 붙어서 동일한지
+def solution(phone_book):    
     hash_map = {}
     for p in phone_book:
         hash_map[p] = 1
     
-    
     for p in phone_book:
-        temp = ''
-        for number in p:
-            temp += number
+        temp = ""
+        for c in p:
+            temp += c
             
             if temp in hash_map and temp != p:
                 return False
-            
-    return answer
+    return True
