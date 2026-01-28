ee0988ea57bb5f64ea753143210981f3d571d2a8
treestone <100207991+TreeStone94@users.noreply.github.com>
2026-01-28 21:30:46 +0900

    [level 2] Title: 타겟 넘버, Time: 372.70 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,11 +1,17 @@
 def solution(numbers, target):
     def dfs(index, current_sum):
-        if index == len(numbers):
-            return 1 if current_sum == target else 0
-        count = 0
-        count += dfs(index+1, current_sum + numbers[index])
+        # 모든 숫자를 다 사용했을 때
+        if index == len(numbers):  # len(numbers)-1이 아니라 len(numbers)!
+            if current_sum == target:
+                return 1
+            else:
+                return 0
         
-        count += dfs(index+1, current_sum - numbers[index])
+        # 현재 숫자를 더하는 경우
+        count1 = dfs(index+1, current_sum + numbers[index])
+        # 현재 숫자를 빼는 경우
+        count2 = dfs(index+1, current_sum - numbers[index])
         
-        return count
-    return dfs(0,0)
+        return count1 + count2
+    
+    return dfs(0, 0)
